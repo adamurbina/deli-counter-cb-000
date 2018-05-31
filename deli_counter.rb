9ba0f64eb39c696332355c0e257347ac77fd77fb
Adam Urbina <adamurbina@gmail.com>
2018-05-31 03:31:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,9 @@ def line(array)
     if array.size == 0
         puts "There is nobody waiting to be served!"
     else
-        
+        message = "The line is currently: "
+        message2 = ""
+        array.each_with_index { |name , index|
+            message2 + (index + 1).to_s + ". " + name
+        }
 end
