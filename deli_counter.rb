6e6cc0b538a215295d840fa1c08e4baedbe6430b
Adam Urbina <adamurbina@gmail.com>
2018-05-31 03:35:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
     if array.size == 0
         puts "The line is currently empty."
     else
-        message = "The line is currently: "
+        message = "The line is currently:"
         message2 = ""
         array.each_with_index { |name , index|
             message2 = message2 + " " + (index + 1).to_s + ". " + name
