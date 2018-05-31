0887bd1aeaa0a1e3be93b218f66f49644b961137
Adam Urbina <adamurbina@gmail.com>
2018-05-31 03:42:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(array, name)
     array << name
-    puts "Welcome #{name}. You are number in #{array[-1]} line."
+    puts "Welcome, #{name}. You are number #{array.size} line."
 end
